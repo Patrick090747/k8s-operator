7be12bb07b31b96d0cada8d2d1337bff8816082d
Patrick090747 <1289453297@qq.com>
2023-08-04 21:50:17 +0800

    controller框架逻辑编写，添加相应注释

--- internal/controller/zwhdeployment_controller.go
+++ internal/controller/zwhdeployment_controller.go
@@ -18,13 +18,14 @@ package controller
 
 import (
 	"context"
-
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	appsv1 "zwh.com/pkg/zwh-deployment/api/v1"
+	myAppsv1 "zwh.com/pkg/zwh-deployment/api/v1"
 )
 
 // ZwhDeploymentReconciler reconciles a ZwhDeployment object
@@ -47,16 +48,82 @@ type ZwhDeploymentReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
 func (r *ZwhDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx, "ZwhDployment", req.NamespacedName)
+	logger.Info("Reconcile is started")
 
 	// TODO(user): your logic here
+	//1.获取资源对象
+	md := new(myAppsv1.ZwhDeployment)
 
+	if err := r.Client.Get(ctx, req.NamespacedName, md); err != nil {
+
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+
+	}
+	//防止污染缓存
+	mdCopy := md.DeepCopy()
+
+	//========处理deployment===========
+	deploy := new(appsv1.Deployment)
+	if err := r.Client.Get(ctx, req.NamespacedName, deploy); err != nil {
+		if errors.IsNotFound(err) {
+			//2.1不存在对象
+			//2.1.1创建deployment
+			r.createDeployment(mdCopy)
+
+		} else {
+
+			return ctrl.Result{}, err
+		}
+	} else {
+		//2.2存在对象
+		//2.2.1更新deployment对象
+		r.updateDeployment(mdCopy)
+	}
+
+	//========处理pod=========
+
+	//========处理service=========
+	//3.获取service资源对象
+	//3.1不存在对象
+	//3.1.1mode为ingres
+	//3.1.1.1创建普通service
+	//3.1.2mode为nodeport
+	//3.1.2.1创建nodeport模式的service
+
+	//3.2存在对象
+	//3.2.1mode为ingress
+	//3.2.1.1创建普通service
+	//3.2.2mode为nodeport
+	//3.2.2.1创建nodeport模式的service
+
+	//========处理ingress=========
+	//4.获取ingress资源对象
+	//4.1不存在对象
+	//4.1.1mode为ingress
+	//4.1.1.1创建ingress
+	//4.1.2mode为nodeport
+	//4.1.2.1退出
+	//4.2存在对象
+	//4.2.1mode为ingress
+	//4.2.1.1更新ingress
+	//4.2.2mode为nodeport
+	//4.2.2.1删除ingress
+	logger.Info("Reconcile is ended")
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ZwhDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appsv1.ZwhDeployment{}).
+		For(&myAppsv1.ZwhDeployment{}).
 		Complete(r)
+}
+
+func (r *ZwhDeploymentReconciler) createDeployment(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) updateDeployment(_ *myAppsv1.ZwhDeployment) {
+
 }
