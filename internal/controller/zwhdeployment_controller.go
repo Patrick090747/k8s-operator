138a187cbe41eb2bb41a7e944762102a0c64520e
Patrick090747 <1289453297@qq.com>
2023-08-06 10:34:50 +0800

    serive和ingress处理逻辑，框架完善，待填充业务逻辑

--- internal/controller/zwhdeployment_controller.go
+++ internal/controller/zwhdeployment_controller.go
@@ -19,11 +19,14 @@ package controller
 import (
 	"context"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"strings"
 
 	myAppsv1 "zwh.com/pkg/zwh-deployment/api/v1"
 )
@@ -81,34 +84,80 @@ func (r *ZwhDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		r.updateDeployment(mdCopy)
 	}
 
-	//========处理pod=========
-
 	//========处理service=========
 	//3.获取service资源对象
-	//3.1不存在对象
-	//3.1.1mode为ingres
-	//3.1.1.1创建普通service
-	//3.1.2mode为nodeport
-	//3.1.2.1创建nodeport模式的service
+	svc := new(corev1.Service)
+	if err := r.Client.Get(ctx, req.NamespacedName, svc); err != nil {
+		if errors.IsNotFound(err) {
+			//3.1不存在对象
+			//3.1.1mode为ingres
 
-	//3.2存在对象
-	//3.2.1mode为ingress
-	//3.2.1.1创建普通service
-	//3.2.2mode为nodeport
-	//3.2.2.1创建nodeport模式的service
+			if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeIngress {
+				//3.1.1.1创建普通service
+				r.createService(mdCopy)
+			} else if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeNodePort {
+				//3.1.2mode为nodeport
+				//3.1.2.1创建nodeport模式的service
+				r.createNPService(mdCopy)
+			} else {
+				return ctrl.Result{}, myAppsv1.ErrorNotSupportMode
+			}
+		} else {
+			return ctrl.Result{}, err
+		}
+	} else {
+		//3.2存在对象
+
+		if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeIngress {
+			//3.2.1mode为ingress
+			//3.2.1.1更新普通service
+			r.updateService(mdCopy)
+		} else if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeNodePort {
+			//3.2.2mode为nodeport
+			//3.2.2.1更新nodeport模式的service
+			r.updateNPSerive(mdCopy)
+
+		} else {
+			return ctrl.Result{}, myAppsv1.ErrorNotSupportMode
+		}
+
+	}
 
 	//========处理ingress=========
 	//4.获取ingress资源对象
-	//4.1不存在对象
-	//4.1.1mode为ingress
-	//4.1.1.1创建ingress
-	//4.1.2mode为nodeport
-	//4.1.2.1退出
-	//4.2存在对象
-	//4.2.1mode为ingress
-	//4.2.1.1更新ingress
-	//4.2.2mode为nodeport
-	//4.2.2.1删除ingress
+	ig := new(networkv1.Ingress)
+	if err := r.Get(ctx, req.NamespacedName, ig); err != nil {
+		if errors.IsNotFound(err) {
+			//4.1不存在对象
+
+			if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeIngress {
+				//4.1.1mode为ingress
+				//4.1.1.1创建ingress
+				r.createIngress(mdCopy)
+			} else if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeNodePort {
+				//4.1.2mode为nodeport
+				//4.1.2.1退出
+				return ctrl.Result{}, nil
+			}
+
+		} else {
+			return ctrl.Result{}, err
+		}
+
+	} else {
+		//4.2存在对象
+		if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeIngress {
+			//4.2.1mode为ingress
+			//4.2.1.1更新ingress
+			r.updateIngress(mdCopy)
+		} else if strings.ToLower(mdCopy.Spec.Expose.Mode) == myAppsv1.ModeNodePort {
+			//4.2.2mode为nodeport
+			//4.2.2.1删除ingress
+			r.deleteIngress(mdCopy)
+		}
+
+	}
+
 	logger.Info("Reconcile is ended")
 	return ctrl.Result{}, nil
 }
@@ -125,5 +174,33 @@ func (r *ZwhDeploymentReconciler) createDeployment(_ *myAppsv1.ZwhDeployment) {
 }
 
 func (r *ZwhDeploymentReconciler) updateDeployment(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) createService(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) createNPService(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) updateService(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) updateNPSerive(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) createIngress(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) updateIngress(_ *myAppsv1.ZwhDeployment) {
+
+}
+
+func (r *ZwhDeploymentReconciler) deleteIngress(_ *myAppsv1.ZwhDeployment) {
 
 }
